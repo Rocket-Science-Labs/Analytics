d8d22c6448e8ce89c5af9df2f0f586945eb96f76
Николай <>
2020-02-11 22:52:30 +0300

    Update analytics

--- Sources/Analytics/EventType.swift
+++ Sources/Analytics/EventType.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 public protocol EventType {
-    public func name(for provider: ProviderType) -> String?
-    public func parameters(for provider: ProviderType) -> [String: Any]?
+    func name(for provider: ProviderType) -> String?
+    func parameters(for provider: ProviderType) -> [String: Any]?
 }
--- Sources/Analytics/ProviderType.swift
+++ Sources/Analytics/ProviderType.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 public protocol ProviderType {
-    public func log(_ eventName: String, parameters: [String: Any]?)
+    func log(_ eventName: String, parameters: [String: Any]?)
 }
